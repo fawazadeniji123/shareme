bb887e1f49c870fdd6e5b2c6e9a84231b9e87d73
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-05-11 09:58:52 +0100

    chore: Update main.jsx to include Google OAuth provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { BrowserRouter as Router } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_API_TOKEN}>
+        <App />
+      </GoogleOAuthProvider>
+    </Router>
   </React.StrictMode>,
 )
