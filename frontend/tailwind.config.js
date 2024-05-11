7ee9a756f4b111a6b38a73ebeb7ee6090061d418
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-05-11 10:31:47 +0100

    chore: Update tailwind.config.js to comment out darkMode option

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  // darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       margin: {
