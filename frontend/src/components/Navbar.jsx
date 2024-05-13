46c9c7e47a49647371f80957c625abce42ad6836
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-05-13 03:25:51 +0100

    feat: Improve Navbar component with search functionality and user profile link
    The Navbar component in the frontend is updated to include a search input field and functionality, allowing users to search for specific content. Additionally, a user profile link is added to provide easy access to the user's profile page. This enhancement improves the user experience by enhancing navigation and search capabilities.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,57 @@
-const Navbar = () => {
+import PropTypes from 'prop-types'
+import { Link, useNavigate } from 'react-router-dom'
+import { IoMdAdd, IoMdSearch } from 'react-icons/io'
+import { MdAccountBox } from 'react-icons/md'
+
+const Navbar = ({ searchTerm, setSearchTerm, user }) => {
+  user = null
+  const navigate = useNavigate()
+
   return (
-    <div>Navbar</div>
+    <div className='flex gap-2 md:gap-5 w-full mt-5 pb-7'>
+      <div className='flex w-full justify-start items-center px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm'>
+        <IoMdSearch fontSize={21} className='ml-1' />
+        <input
+          type='search'
+          placeholder='Search...'
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+          onFocus={() => navigate('/search')}
+          className='w-full p-2 outline-none bg-white'
+        />
+      </div>
+      <div className='flex gap-3'>
+        {user ? (
+          <Link to={`user-profile/${user?._id}`} className='hidden md:block'>
+            <img
+              src={user?.image}
+              alt='User Profile'
+              className='w-14 h-12 rounded-lg justify-center items-center'
+            />
+          </Link>
+        ) : (
+          <Link
+            to={'/login'}
+            className='w-12 h-12 md:w-14 md:h-12 rounded-lg flex justify-center items-center'
+          >
+            <MdAccountBox fontSize={60} />
+          </Link>
+        )}
+        <Link
+          to='/create-pin'
+          className='bg-black text-white rounded-lg w-12 h-12 md:w-14 md:h-12 flex justify-center items-center'
+        >
+          <IoMdAdd />
+        </Link>
+      </div>
+    </div>
   )
+}
+
+Navbar.propTypes = {
+  searchTerm: PropTypes.string,
+  setSearchTerm: PropTypes.func,
+  user: PropTypes.object,
 }
 
 export default Navbar
