ce6d377f308f0143f4b4deba641cefd4d8184c3e
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-05-13 02:17:07 +0100

    feat: Update Pins component to include Navbar and Routes
    The Pins component in the frontend is updated to include the Navbar component and Routes for different sections of the application. This enhancement improves the user experience by providing a navigation bar and allowing users to easily switch between different views, such as the feed, pin details, and pin creation.

--- frontend/src/containers/Pins.jsx
+++ frontend/src/containers/Pins.jsx
@@ -1,7 +1,43 @@
-const Pins = () => {
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+import { Navbar, Feed, PinDetail, CreatePin, Search } from '../components'
+
+const Pins = ({ user }) => {
+  const [searchTerm, setSearchTerm] = useState('')
+
   return (
-    <div>Pins</div>
+    <div className='px-2 md:px-5'>
+      <div className='bg-gray-50'>
+        <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+      <div className='h-full'>
+        <Routes>
+          <Route path='/' element={<Feed searchTerm={searchTerm} />} />
+          <Route
+            path='/categories/:categoryId'
+            element={<Feed searchTerm={searchTerm} />}
+          />
+          <Route
+            path='/search'
+            element={
+              <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            }
+          />
+          <Route
+            path='/pin-detail/:pinId'
+            element={<PinDetail user={user} />}
+          />
+          <Route path='/create-pin' element={<CreatePin user={user} />} />
+        </Routes>
+      </div>
+    </div>
   )
+}
+
+Pins.propTypes = {
+  user: PropTypes.object,
 }
 
 export default Pins
