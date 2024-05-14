5122e58be177ed2f51a2eafcb4a093964005d371
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-05-14 08:42:19 +0100

    feat: Add Spinner component to frontend
    The Spinner component is added to the frontend, providing a loading indicator for asynchronous operations. This enhancement improves the user experience by visually indicating when content is being loaded.

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+import { Circles } from 'react-loader-spinner'
+
+const Spinner = ({ message }) => {
+  return (
+    <div className='flex flex-col justify-center items-center w-full h-full'>
+      <Circles
+        color='#00BFFF'
+        height={'50'}
+        width={'200'}
+        wrapperClass='m-5'
+        visible={true}
+      />
+      <p className='text-lg text-center px-2'>{message}</p>
+    </div>
+  )
+}
+
+Spinner.propTypes = {
+  message: PropTypes.string.isRequired,
+}
+
+export default Spinner
