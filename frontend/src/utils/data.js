e97a85c5e03b9a4b1acaf4cc0b5f1b4a969ee02b
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-05-14 08:41:53 +0100

    refactor: Update userQuery, userPinsQuery, pinQuery, searchQuery, and feedQuery functions in data.js
    The userQuery, userPinsQuery, pinQuery, searchQuery, and feedQuery functions in the data.js file are updated to improve the querying logic and optimize the retrieval of pin data. This refactor enhances the performance and efficiency of the application's data retrieval process.

--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -1,5 +1,131 @@
 export const userQuery = userId => {
-  const query = `*[_type == "user" && _id == "${userId}"]`;
+  const query = `*[_type == "user" && _id == "${userId}"]`
 
-  return query;
+  return query
 }
+
+export const userPinsQuery = userId => {
+  const query = `*[_type == "pin" && postedBy._ref == "${userId}"] | order(_createdAt desc){
+    image {
+      asset-> {
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy-> {
+      _id,
+      userName,
+      image {
+        asset-> {
+          url
+        }
+      }
+    },
+    save[] {
+      _key,
+      postedBy-> {
+        _id
+        userName,
+        image 
+        }
+      }
+    }
+  }`
+
+  return query
+}
+
+export const pinQuery = pinId => {
+  const query = `*[_type == "pin" && _id == "${pinId}"] {
+    image {
+      asset-> {
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy-> {
+      _id,
+      userName,
+      image {
+        asset-> {
+          url
+        }
+      }
+    },
+    save[] {
+      _key,
+      postedBy-> {
+        _id
+        userName,
+        image 
+        }
+      }
+    }
+  }`
+
+  return query
+}
+
+export const searchQuery = searchTerm => {
+  const query = `*[_type == "pin" && title match "${searchTerm}*" || category match "${searchTerm}*" || about match "${searchTerm}*"] | order(_createdAt desc){
+    image {
+      asset-> {
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy-> {
+      _id,
+      userName,
+      image {
+        asset-> {
+          url
+        }
+      }
+    },
+    save[] {
+      _key,
+      postedBy-> {
+        _id
+        userName,
+        image 
+        }
+      }
+    }
+  }`
+
+  return query
+}
+
+export const feedQuery = `*[_type == "pin"] | order(_createdAt desc){
+    image {
+      asset-> {
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy-> {
+      _id,
+      userName,
+      image {
+        asset-> {
+          url
+        }
+      }
+    },
+    save[] {
+      _key,
+      postedBy-> {
+        _id
+        userName,
+        image 
+        }
+      }
+    }
+  }`
+
+
