63eb290c4bdb3e6ac7bed3b74bb66bfad17466c5
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-05-14 08:50:39 +0100

    feat: Update MasonryLayout component to optimize pin rendering
    The MasonryLayout component is updated to optimize the rendering of pins in the masonry grid. This improvement enhances the performance and efficiency of pin display, resulting in a smoother user experience.

--- frontend/src/components/MasonryLayout.jsx
+++ frontend/src/components/MasonryLayout.jsx
@@ -1,18 +1,34 @@
-import PropTypes from 'prop-types';
-import Masonry from 'react-masonry-css';
+import PropTypes from 'prop-types'
+import Masonry from 'react-masonry-css'
 
-import Pin from './Pin';
+import Pin from './Pin'
 
-const MasonryLayout = ({pins}) => {
+const breakpointColumnsObj = {
+  default: 4,
+  3000: 6,
+  2000: 5,
+  1200: 3,
+  700: 2,
+  500: 1,
+}
+
+const MasonryLayout = ({ pins }) => {
   return (
     <div>
-      
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className='flex animate-slide-fwd'
+      >
+        {pins?.map(pin => (
+          <Pin key={pin._id} pin={pin} className='w-max' />
+        ))}
+      </Masonry>
     </div>
   )
 }
 
 MasonryLayout.propTypes = {
-  pins: PropTypes.array.isRequired
+  pins: PropTypes.array.isRequired,
 }
 
 export default MasonryLayout
