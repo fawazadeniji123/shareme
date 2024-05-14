d92d8058c7bede3a9b92fe2888f36af5c8faa0b1
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-05-14 08:43:09 +0100

    feat: Add MasonryLayout component to frontend
    The MasonryLayout component is added to the frontend, providing a layout for displaying pins in a masonry grid. This enhancement improves the visual presentation of pins and enhances the user experience by creating an organized and visually appealing layout.

---
+++ frontend/src/components/MasonryLayout.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import Masonry from 'react-masonry-css';
+
+import Pin from './Pin';
+
+const MasonryLayout = ({pins}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+MasonryLayout.propTypes = {
+  pins: PropTypes.array.isRequired
+}
+
+export default MasonryLayout
