bf63908273f40aac894d1a728976a30a27c4ca9a
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-05-14 08:43:23 +0100

    feat: Add MasonryLayout component and update Feed component
    The MasonryLayout component is added to the frontend, providing a layout for displaying pins in a masonry grid. This enhancement improves the visual presentation of pins and enhances the user experience by creating an organized and visually appealing layout.
    
    The Feed component is updated to use the MasonryLayout component and fetch pins based on the selected category. This improvement ensures that the pins are displayed in the new masonry grid layout and provides a more tailored feed for users based on their category selection.

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,6 +1,58 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+import { client } from '../client'
+import { feedQuery, searchQuery } from '../utils/data'
+import MasonryLayout from './MasonryLayout'
+import Spinner from './Spinner'
+
 const Feed = () => {
+  const [loading, setLoading] = useState(false)
+  const [pins, setPins] = useState([])
+  const { categoryId } = useParams()
+
+  // ! TODO: search for users
+
+  useEffect(() => {
+    setLoading(true)
+
+    if (categoryId) {
+      const query = searchQuery(categoryId)
+      client
+        .fetch(query)
+        .then(data => {
+          setPins(data)
+          setLoading(false)
+        })
+        .catch(error => {
+          console.error(error)
+          setLoading(false)
+        })
+    } else {
+      client
+        .fetch(feedQuery)
+        .then(data => {
+          setPins(data)
+          setLoading(false)
+        })
+        .catch(error => {
+          console.error(error)
+          setLoading(false)
+        })
+    }
+  }, [categoryId])
+
+  if (loading)
+    return <Spinner message='we are adding new ideas to your feed...' />
   return (
-    <div>Feed</div>
+    <div>
+      {pins ? (
+        <MasonryLayout pins={pins} />
+      ) : (
+        <h1 className='text-center mt-5'>No pins available</h1>
+      )}
+    </div>
   )
 }
 
