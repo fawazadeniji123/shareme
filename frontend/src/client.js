72df86f54c2abfde8a861317973b8b580a8bf6e7
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-05-15 13:01:53 +0100

    chore: Update Sanity API version to v2022-03-07
    The Sanity API version in the client.js file is updated to v2022-03-07. This update ensures compatibility with the latest version of the Sanity API, providing access to new features, bug fixes, and performance improvements.

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -4,7 +4,7 @@ import imageUrlBuilder from '@sanity/image-url/'
 export const client = createClient({
   projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
   dataset: 'production',
-  apiVersion: 'v1',
+  apiVersion: 'v2022-03-07',
   useCdn: true,
   token: import.meta.env.VITE_SANITY_API_TOKEN,
 })
