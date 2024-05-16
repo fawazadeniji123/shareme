71857007e24c642670f0ce62a6909e32e23411a9
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-05-16 13:20:55 +0100

    feat: Refactor Pin component to handle saving and unsaving pins
    The Pin component in Pin.jsx is refactored to handle the saving and unsaving of pins. The savePin function is updated to add or remove the user's saved pin from the save list. This improvement enhances the user experience by allowing them to easily save and unsave pins with a single click.

--- frontend/src/components/Pin.jsx
+++ frontend/src/components/Pin.jsx
@@ -3,20 +3,90 @@ import PropTypes from 'prop-types'
 import { Link, useNavigate } from 'react-router-dom'
 import { v4 as uuidv4 } from 'uuid'
 import { MdDownloadForOffline } from 'react-icons/md'
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
 import { AiTwotoneDelete } from 'react-icons/ai'
 import { BsFillArrowUpCircleFill } from 'react-icons/bs'
 
 import { client, urlFor } from '../client'
+import { pinQuery } from '../utils/data'
 import { fetchUser } from '../utils/fetchUser'
 
+const savePin = (pinId, user, alreadySaved, navigate, save, setSaveList) => {
+  if (!user) {
+    navigate('/login')
+    return
+  }
+
+  if (!alreadySaved) {
+    try {
+      client
+        .patch(pinId)
+        .setIfMissing({ save: [] })
+        .insert('after', 'save[-1]', [
+          {
+            _key: uuidv4(),
+            userid: user?.sub,
+            postedBy: {
+              _type: 'postedBy',
+              _ref: user?.sub,
+            },
+          },
+        ])
+        .commit()
+        .then(() => {
+          client.fetch(pinQuery(pinId)).then(data => {
+            console.log(data)
+            setSaveList(data[0].save)
+          })
+        })
+    } catch (error) {
+      console.error(error)
+    }
+  } else {
+    try {
+      const saved = save?.find(item => item?.postedBy?._id === user?.sub)
+      const saveId = saved?._key
+      console.log(saveId)
+
+      client
+        .patch(pinId)
+        .unset([`save[_key=="${saveId}"]`])
+        .commit()
+        .then(() => {
+          client.fetch(pinQuery(pinId)).then(data => {
+            console.log(data)
+            setSaveList(data[0].save)
+          })
+        })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
+const handleSave =
+  (pinId, user, alreadySaved, navigate, saveList, setSaveList) => e => {
+    e.stopPropagation()
+    e.target.disabled = true
+
+    savePin(pinId, user, alreadySaved, navigate, saveList, setSaveList)
+
+    e.target.disabled = false
+  }
+
 const Pin = ({ pin: { image, postedBy, _id, destination, save } }) => {
+  const [saveList, setSaveList] = useState(save)
   const [postHovered, setPostHovered] = useState(false)
-  const [savingPost, setSavingPost] = useState(false)
   const navigate = useNavigate()
+
+  // check if device is tablet or mobile
+  const isMoblieOrTablet =
+    window.innerWidth < 1024 && window.matchMedia('(pointer: coarse)').matches
 
   const user = fetchUser()
 
-  const alreadySaved = save?.includes(user.sub)
+  const alreadySaved = saveList?.some(item => item?.postedBy?._id === user?.sub)
+  // console.log(save, user.sub, alreadySaved)
 
   return (
     <div className='m-2 '>
@@ -31,7 +101,7 @@ const Pin = ({ pin: { image, postedBy, _id, destination, save } }) => {
           alt='User post'
           className='rounded-lg w-full'
         />
-        {postHovered && (
+        {(postHovered || isMoblieOrTablet) && (
           <div
             style={{ height: '100%' }}
             className='absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50'
@@ -62,7 +132,41 @@ const Pin = ({ pin: { image, postedBy, _id, destination, save } }) => {
                   <MdDownloadForOffline />
                 </a>
               </div>
-              {alreadySaved ? <button>Saved</button> : <button>Save</button>}
+              {alreadySaved ? (
+                <button
+                  className='flex items-center justify-between opacity-70 text-red-600 hover:opacity-100 font-bold px-1 py-1 text-base rounded-3xl hover:shadow-md outline-none transition-all duration-100 ease-in'
+                  onClick={handleSave(
+                    _id,
+                    user,
+                    alreadySaved,
+                    navigate,
+                    saveList,
+                    setSaveList,
+                  )}
+                >
+                  <AiFillHeart className='text-lg' />
+                  <span className='px-1'>
+                    {saveList?.length ? saveList?.length : null}
+                  </span>
+                </button>
+              ) : (
+                <button
+                  className='flex items-center justify-between opacity-70 text-white hover:text-red-600 hover:opacity-100 font-bold px-1 py-1 text-base rounded-3xl hover:shadow-md outline-none transition-all duration-100 ease-in'
+                  onClick={handleSave(
+                    _id,
+                    user,
+                    alreadySaved,
+                    navigate,
+                    saveList,
+                    setSaveList,
+                  )}
+                >
+                  <AiOutlineHeart className=' text-lg' />
+                  <span className='px-1'>
+                    {saveList?.length ? saveList?.length : null}
+                  </span>
+                </button>
+              )}
             </div>
           </div>
         )}
