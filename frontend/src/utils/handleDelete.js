01fe9e84dbce899743b903b280e8b2655ca6ef06
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-05-18 07:02:52 +0100

    chore: formatted file

--- frontend/src/utils/handleDelete.js
+++ frontend/src/utils/handleDelete.js
@@ -1,11 +1,10 @@
 import { deletePin } from './deletePin'
 
-export const handleDelete =
-  (pinId, setSaveList) => async e => {
-    e.stopPropagation()
-    e.target.disabled = true
+export const handleDelete = (pinId, setSaveList) => async e => {
+  e.stopPropagation()
+  e.target.disabled = true
 
-    await deletePin(pinId, setSaveList)
+  await deletePin(pinId, setSaveList)
 
-    e.target.disabled = false
-  }
+  e.target.disabled = false
+}
