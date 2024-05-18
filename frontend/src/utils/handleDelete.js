d7089240ed8c1034233d5abc1e2b1eec00a215f5
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-05-18 06:51:26 +0100

    feat: Add handleDelete function to handle pin deletion

---
+++ frontend/src/utils/handleDelete.js
@@ -0,0 +1,11 @@
+import { deletePin } from './deletePin'
+
+export const handleDelete =
+  (pinId, setSaveList) => async e => {
+    e.stopPropagation()
+    e.target.disabled = true
+
+    await deletePin(pinId, setSaveList)
+
+    e.target.disabled = false
+  }
